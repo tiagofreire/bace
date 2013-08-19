643cf7c3ae92d524e13aacba6ebae672f2ed68da
Tiago Freire <tiagowd@gmail.com>
2013-08-19 11:48:44 -0300

    pasta com template

--- modulos/automacao/admin.py
+++ modulos/automacao/admin.py
@@ -11,6 +11,11 @@ class NotaFiscalAdmin(admin.ModelAdmin):
   list_display=('numero','peso')
   list_per_page = 25
   search_fields = ['numero',]
-	
+	                            
+class OrdemFabricacaoAdmin(admin.ModelAdmin):
+	list_display=('nota_fiscal','operador','produto','peso_bruto','bitola','previsao')
+	list_per_page = 25
+	search_fields = ['nota_fiscal','operador','produto','peso_bruto','bitola','previsao']
 admin.site.register(Material,MaterialAdmin)
-admin.site.register(NotaFiscal,NotaFiscalAdmin)
+admin.site.register(NotaFiscal,NotaFiscalAdmin)                                       
+admin.site.register(OrdemFabricacao,OrdemFabricacaoAdmin)                                       
--- modulos/automacao/models.py
+++ modulos/automacao/models.py
@@ -1,7 +1,9 @@
 #-*- coding: utf-8 -*-
 from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 from django.db.models import signals
+
 
 class Material(models.Model):
   descricao = models.CharField(u"Descrição",max_length=200, blank=False, null=False)
@@ -13,6 +15,19 @@ class Material(models.Model):
     def __str__(self):
       return "%s" % self.descricao
     
+    def __unicode__(self):
+        return "%s" % self.descricao 
+
+class Produto(models.Model):
+  descricao = models.CharField(u"Descrição",max_length=200, blank=False, null=False)
+
+  class Meta:
+    verbose_name = "Produto"
+    verbose_name_plural  = "Produtos"
+
+    def __str__(self):
+      return "%s" % self.descricao
+
     def __unicode__(self):
         return "%s" % self.descricao
 
@@ -30,4 +45,21 @@ class NotaFiscal(models.Model):
 
     def __unicode__(self):
         return "%s" % self.numero
+                                             
+class OrdemFabricacao(models.Model):
+	nota_fiscal = models.ForeignKey("Nota Fiscal", NotaFiscal, blank = False, null = False)
+	operador = models.ForeignKey("Operador",User, blank=False, null=False)
+	#produto = models.ForeignKey("Produto",Produto, blank=False, null=False)
+	peso_bruto = models.CharField("Peso Bruto",max_length=10,blank=False, null=False)
+	bitola = models.CharField("Bitola",max_length=10,blank=False, null=False)
+	previsao = models.CharField(u"Previsão",max_length=10,blank=False, null=False)   
+	
+	class Meta:
+    verbose_name = u"Ordem de Fabricação"
+    verbose_name_plural  = u"Ordens de Fabricação"
 
+    def __str__(self):
+      return "%s" % self.nota_fiscal.numero
+
+    def __unicode__(self):
+      return "%s" % self.nota_fiscal.numero
