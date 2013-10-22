3c12d4c4cf9faa225843a66266982a4e954c5231
Tiago Freire <tiagowd@gmail.com>
2013-10-22 01:27:29 -0300

    tirando obritatorio dos campso

--- baceerp/modulos/automacao/models.py
+++ baceerp/modulos/automacao/models.py
@@ -25,12 +25,12 @@ class MaterialNotaFiscal(models.Model):
      ("2", "Finalizado"),
    )  
   nota_fiscal = models.ForeignKey(NotaFiscal)
-  material = models.ForeignKey(Material,blank=False,null=False)
-  volume = models.CharField(u"Volume", max_length=100,blank=False,null=False)
-  data_entrada = models.DateField(u"Data de Entrada", max_length=100,blank=False,null=False)
-  peso = models.DecimalField(u"Peso", max_length=100,max_digits=8,decimal_places=2,blank=False,null=False)
-  valor = models.DecimalField(u"Valor", max_digits=8,decimal_places=2,blank=False,null=False)
-  status = models.CharField("Status",max_length=100, choices=STATUS_MATERIAL,blank=True,null=True)
+  material = models.ForeignKey(Material)
+  volume = models.CharField(u"Volume", max_length=100)
+  data_entrada = models.DateField(u"Data de Entrada", max_length=100)
+  peso = models.DecimalField(u"Peso", max_length=100,max_digits=8,decimal_places=2)
+  valor = models.DecimalField(u"Valor", max_digits=8,decimal_places=2)
+  status = models.CharField("Status",max_length=100, choices=STATUS_MATERIAL)
 
   class Meta:
     verbose_name = "Material"
