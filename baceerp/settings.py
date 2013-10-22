56b8d1d6a1f2c1a17bd596b9eb046d29c3dc7dd1
Tiago Freire <tiagowd@gmail.com>
2013-10-21 23:56:53 -0300

    libs do banco

--- baceerp/settings.py
+++ baceerp/settings.py
@@ -118,7 +118,7 @@ INSTALLED_APPS = (
     # instalados
     'django.contrib.admin',
     'bootstrap_admin',
-    'south',
+    #'south',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 
