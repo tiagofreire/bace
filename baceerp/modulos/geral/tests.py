f4f0957018e6e9f8822d0fd75d71a7d04ea243c2
root <root@tiago>
2013-11-14 13:41:23 -0300

    teste modulo geral

--- baceerp/modulos/geral/tests.py
+++ baceerp/modulos/geral/tests.py
@@ -1,16 +1,46 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
+#-*- coding: utf-8 -*-
 from django.test import TestCase
+from baceerp.modulos.geral.models import TipoMaterial, Material,GrupoProduto,Produto
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class GeralTest(TestCase):
+  def test_cria_tipo_material(self):
+    tipo_material = TipoMaterial()
+    tipo_material.descricao = "Material 01"
+    tipo_material.save()
+    
+    list_tipo_material = TipoMaterial.objects.all()
+    self.assertEquals(len(list_tipo_material),1)
+    self.assertEquals(list_tipo_material[0].descricao,"Material 01")
+    
+  def test_cria_material(self):
+  
+    tipo_material = TipoMaterial(
+      descricao = "Tipo Material 01"
+    )
+    tipo_material.save()
+    
+    material = Material(
+      tipo_material = tipo_material,
+      codigo = "0",
+      descricao = "Material 01"
+    )
+    material.save()
+    list_material = Material.objects.all()
+    self.assertEquals(len(list_material),1)
+    self.assertEquals(list_material[0].tipo_material.descricao,"Tipo Material 01")
+    self.assertEquals(list_material[0].descricao,"Material 01")
+    
+  def test_produto(self):
+    grupo_produto = GrupoProduto(
+      codigo = 0,
+      descricao = "Grupo Produto 01"
+    )
+    grupo_produto.save()
+    
+    produto = Produto(
+      grupo_produto = grupo_produto,
+      codigo = 0,
+      descricao = "Produto 01"
+    )
+    produto.save()
+    self.assertEquals(produto.codigo, "1.1")
